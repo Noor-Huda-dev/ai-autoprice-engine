9a778a069916c3a9cc915ef9f2b0c595ca3b0e34
Noor <nh8746945@gmail.com>
2026-05-26 01:03:21 +0500

    fix dependencies

--- tariqworldwide/settings.py
+++ tariqworldwide/settings.py
@@ -98,7 +98,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'dresses.context_processors.cart_items_count',
+                #'dresses.context_processors.cart_items_count',
 
             ],
             'libraries': {

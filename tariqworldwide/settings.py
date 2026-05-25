d9e37b053d5ca5cfcb2e5c6d57d5e8a9189c3bf6
Noor <nh8746945@gmail.com>
2026-05-25 17:15:05 +0500

    move secret key to environment variables

--- tariqworldwide/settings.py
+++ tariqworldwide/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*v!abuzu7mtbl3i!dlicw2e3doycbi5cuf(40tf)bk_0abdeg('
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".railway.app", "*"]
 
 
 
@@ -154,7 +153,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-DEBUG = True
+DEBUG = False
 
 STATIC_URL = '/static/'
 
@@ -174,8 +173,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-

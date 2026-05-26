f7460f6900730b0e99dc28716219badc21c88cd3
Noor <nh8746945@gmail.com>
2026-05-26 08:41:55 +0500

    temporary test route

--- tariqworldwide/urls.py
+++ tariqworldwide/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def test(request):
+    return HttpResponse("WORKING")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dresses.urls')),
+    path('', test),
     path('accounts/', include('allauth.urls')),
 ]
 
